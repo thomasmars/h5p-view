befce048e3afb43cdadf838f9e2ac9d18eaf049a
Thomas Marstrander <thomasmars87@gmail.com>
2016-03-29 14:53:13 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by thoma_000 on 29.03.2016.
+ */
+require('./scripts/jquery');
+require('./scripts/h5p');
+require('./scripts/h5p-event-dispatcher');
+require('./scripts/h5p-x-api-event');
+require('./scripts/h5p-x-api');
+require('./scripts/h5p-content-type');
+
+module.exports = H5P;
+
---
+++ scripts/init.js
@@ -0,0 +1,5 @@
+/**
+ * Created by thoma_000 on 21.03.2016.
+ */
+
+var H5PIntegration = {};
