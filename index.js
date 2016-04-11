beb20dc7f4da2fe1bfbc2135ee1a13a4d2c64907
Thomas Marstrander <thomasmars87@gmail.com>
2016-04-11 18:36:48 +0200

    Made it easier to update H5P library by including the submodule. Included raw H5P scripts with webpack, without having to change them. Included H5P gif and fonts.

--- index.js
+++ index.js
@@ -2,15 +2,21 @@
  * Created by thoma_000 on 29.03.2016.
  */
 
-// Requires H5P to be a global variable.
+// Assets
+require('./library/images/throbber.gif');
+
+// Inline css
+require('./library/styles/h5p.css');
+require('./library/styles/h5p-confirmation-dialog.css');
+require('./library/styles/h5p-core-button.css');
+
 global.H5PIntegration = {};
-global.H5P = {};
 
-require('./scripts/h5p');
-require('./scripts/h5p-event-dispatcher');
-require('./scripts/h5p-x-api-event');
-require('./scripts/h5p-x-api');
-require('./scripts/h5p-content-type');
-
-document.write('TESTING!!');
+// Require js
+require('script!./library/js/jquery');
+require('script!./library/js/h5p');
+require('script!./library/js/h5p-event-dispatcher');
+require('script!./library/js/h5p-x-api-event');
+require('script!./library/js/h5p-x-api');
+require('script!./library/js/h5p-content-type');
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,31 @@ module.exports = {
     path: path.join(__dirname, 'dist'),
     filename: "bundle.js"
   },
+  module: {
+    loaders: [
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)(\?.*$|$)/,
+        include: path.resolve(__dirname, 'library/fonts'),
+        loader: 'file'
+      },
+      {
+        test: /\.(gif)(\?.*$|$)/,
+        include: path.resolve(__dirname, 'library/images'),
+        loader: 'file'
+      },
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, 'library/styles'),
+        loader: 'style!css'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      'H5P': './h5p.js',
+      'Event': './h5p-event-dispatcher.js'
+    })
+  ],
   devServer: {
     port: 8050
   }
